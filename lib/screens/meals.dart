d8b5695a0fee3e929f0c36d97f2dcdf3293c0a1b
Adekams <kifademi@gmail.com>
2023-08-17 00:06:15 +0100

    building meals screen

---
+++ lib/screens/meals.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.mealTitle, required this.meals});
+
+  final String mealTitle;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, indx) {
+        return Text(
+          meals[indx].title,
+        );
+      },
+    );
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('images/Oops.png'),
+            Text(
+              'Oops! No meal found',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Please select a different category',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(mealTitle),
+      ),
+      body: content,
+    );
+  }
+}
