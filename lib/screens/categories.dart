e9323ae210342f97932deaca195a7b424770cfd9
Adekams <kifademi@gmail.com>
2023-08-18 23:51:14 +0100

    category selection

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(mealTitle: 'meal Title', meals: []),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meal Categories'),
+        title: const Text('Select a Meal Category'),
       ),
       body: GridView(
         padding: const EdgeInsets.all(15),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
+          /*childAspectRatio specifies each card width -can also be set to 1.5*/
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category),
+            CategoryGridItem(category,
+                onSelectCategory: () => _selectCategory(context)),
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem(this.category, {super.key});
+  const CategoryGridItem(this.category,
+      {super.key, required this.onSelectCategory});
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(15),
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
           gradient: LinearGradient(
             colors: [
               category.color.withOpacity(.5),
