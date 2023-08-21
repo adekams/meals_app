79d1f070cfeeb2d8847048885312492dcd2a5896
Adekams <kifademi@gmail.com>
2023-08-21 01:23:51 +0100

    setting up single meal screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meal_detail.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 import '../models/meal.dart';
@@ -9,6 +10,16 @@ class MealsScreen extends StatelessWidget {
   final String mealTitle;
   final List<Meal> meals;
 
+  void _selectedMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
@@ -16,6 +27,9 @@ class MealsScreen extends StatelessWidget {
       itemBuilder: (ctx, indx) {
         return MealItem(
           meal: meals[indx],
+          onSelectMeal: () {
+            _selectedMeal(context, meals[indx]);
+          },
         );
       },
     );
