99fc5c7d81f9907d453fce259eac662c6781a9c5
Adekams <kifademi@gmail.com>
2023-08-23 02:13:01 +0100

    updating meal detail

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -12,11 +12,64 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        fit: BoxFit.cover,
-        height: 100,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              // height: 100,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+
+            for (final ingredient in meal.ingredients)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  ingredient,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ),
+
+            // steps
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
